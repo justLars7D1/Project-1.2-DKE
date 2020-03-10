96ab12862a4c6ca476ee8c25ae4bd4e527becbbe
justLars7D1 <Larsquaedvlieg@outlook.com>
2020-03-10 11:10:50 +0100

    Update Vector2d.java

--- Code/src/PhysicsEngine/Vector2d.java
+++ Code/src/PhysicsEngine/Vector2d.java
@@ -1,7 +1,21 @@
 package PhysicsEngine;
 
 public class Vector2d {
-//  public Vector2d(double x, double y);
-//  public double get_x();
-//  public double get_y();
+
+    private double x;
+    private double y;
+
+    public Vector2d(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double get_x() {
+        return x;
+    }
+
+    public double get_y() {
+        return y;
+    }
+
 }
