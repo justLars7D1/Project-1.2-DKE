91f819ba91a34326f0337dd8d67f4dc96a42391a
justLars7D1 <Larsquaedvlieg@outlook.com>
2020-03-13 01:27:53 +0100

    Created a better solution for evaluating a reverse polar equation

--- core/src/project12/physicsengine/functions/FunctionParser.java
+++ core/src/project12/physicsengine/functions/FunctionParser.java
@@ -83,78 +83,6 @@ public class FunctionParser implements Function2d {
     }
 
     /**
-     * Evaluates the function
-     * @param p The (x,y)-coordinate
-     * @return The z-coordinate
-     */
-    @Override
-    public double evaluate(Vector2d p) {
-        double xVal = p.get_x();
-        double yVal = p.get_y();
-        //The polish notation in an arraylist, so we can manipulate it later
-        ArrayList<String> pol = new ArrayList<>(Arrays.asList(reversePolishOrder));
-        pol.removeAll(Arrays.asList("", null));
-
-        int i = pol.size()-1;
-        //While there are still calculations to compute
-        while(pol.size() != 1) {
-
-            // If the value is an operator sine, cosine or tangent, and there is a number before it, execute it
-            if (ops.containsKey(pol.get(i)) && !ops.containsKey(pol.get(i-1)) && (pol.get(i).equals("sin") || pol.get(i).equals("cos") || pol.get(i).equals("tan"))) {
-
-                //Parse the values correctly to doubles first and compute the value
-                if (pol.get(i-1).equals("x")) pol.set(i-1, String.valueOf(xVal));
-                else if (pol.get(i-1).equals("y")) pol.set(i-1, String.valueOf(yVal));
-                double valToEval = (pol.get(i-1).equals("e")) ? Math.E: Double.parseDouble(pol.get(i-1));
-                double value = executeOp(pol.get(i), valToEval, 0);
-
-                //Remove the old values and place the new value in the correct position of the arraylist
-                pol.remove(i);
-                pol.remove(i-1);
-                pol.add(i-1, String.valueOf(value));
-
-                //Update the index for the next best operation to perform
-                while(i < pol.size() && !ops.containsKey(pol.get(i))) i++;
-
-//                System.out.println(pol + " : " + i);
-
-            // Else if it's an operator, but not one of the previous ones and there are two number in front, execute it
-            } else if (ops.containsKey(pol.get(i)) && !ops.containsKey(pol.get(i-1)) && !ops.containsKey(pol.get(i-2))) {
-
-                //Parse the values correctly to doubles first and compute the value
-                if (pol.get(i-1).equals("x")) pol.set(i-1, String.valueOf(xVal));
-                else if (pol.get(i-1).equals("y")) pol.set(i-1, String.valueOf(yVal));
-                if (pol.get(i-2).equals("x")) pol.set(i-2, String.valueOf(xVal));
-                else if (pol.get(i-2).equals("y")) pol.set(i-2, String.valueOf(yVal));
-                double valToEval1 = (pol.get(i-1).equals("e")) ? Math.E: Double.parseDouble(pol.get(i-1));
-                double valToEval2 = (pol.get(i-2).equals("e")) ? Math.E: Double.parseDouble(pol.get(i-2));
-                double value = executeOp(pol.get(i), valToEval2, valToEval1);
-
-                //Update the index for the next best operation to perform
-                pol.remove(i);
-                pol.remove(i-1);
-                pol.remove(i-2);
-                pol.add(i-2, String.valueOf(value));
-
-                //Update the index for the next best operation to perform
-                i--;
-                while(i < pol.size() && !ops.containsKey(pol.get(i))) i++;
-
-//                System.out.println(pol + " : " + i);
-
-                //Otherwise continue our search
-            } else {
-                i--;
-            }
-
-        }
-
-        //Return the result
-        return Double.parseDouble(pol.get(0));
-
-    }
-
-    /**
      * Compute the gradient at a point of the function
      * @param p The (x,y)-coordinate
      * @return The gradient
@@ -167,6 +95,37 @@ public class FunctionParser implements Function2d {
         return new Vector2d((zphx-z)/ACCURACYGRADIENTFACTOR, (zphy-z)/ACCURACYGRADIENTFACTOR);
     }
 
+    //Check if the current operator has a higher precedence
+    private static boolean hasHigherPrecedence(String operator, String sub) {
+        return (ops.containsKey(sub) && ops.get(sub).precedence >= ops.get(operator).precedence);
+    }
+
+    /**
+     * Evaluates the function
+     * @param p The (x,y)-coordinate
+     * @return The z-coordinate
+     */
+    @Override
+    public double evaluate(Vector2d p) {
+        Stack<String> stack = new Stack<>();
+        for (String op : reversePolishOrder) {
+            if (op.equals("sin") || op.equals("cos") || op.equals("tan")) {
+                double x = getValue(stack.pop(), p);
+                String result = String.valueOf(executeOp(op, x, 0));
+                stack.push(result);
+            } else if (ops.containsKey(op)) {
+                double y = getValue(stack.pop(), p);
+                double x = getValue(stack.pop(), p);
+                String result = String.valueOf(executeOp(op, x, y));
+                stack.push(result);
+            } else {
+                stack.push(op);
+            }
+        }
+        return Double.parseDouble(stack.pop());
+    }
+
+    //Executes the operation
     private static double executeOp(String operation, double v1, double v2) {
         double val = 0;
         switch (operation) {
@@ -198,9 +157,31 @@ public class FunctionParser implements Function2d {
         return val;
     }
 
-    //Check if the current operator has a higher precedence
-    private static boolean hasHigherPrecedence(String operator, String sub) {
-        return (ops.containsKey(sub) && ops.get(sub).precedence >= ops.get(operator).precedence);
+    //Calculate the value of a stack number (no operation)
+    private double getValue(String stackVal, Vector2d p) {
+        double x;
+        switch (stackVal) {
+            case "e":
+                x = Math.E;
+                break;
+            case "x":
+                x = p.get_x();
+                break;
+            case "y":
+                x = p.get_y();
+                break;
+            default:
+                x = Double.parseDouble(stackVal);
+                break;
+        }
+        return x;
     }
+
+    //For some testing
+//    public static void main(String[] args) {
+//        FunctionParser p = new FunctionParser("cos ( x + y )");
+//        System.out.println(p.evaluate(new Vector2d(0,1)));
+//    }
+
 
 }
