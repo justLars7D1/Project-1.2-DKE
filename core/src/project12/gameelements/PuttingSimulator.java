de05589c7fd46e444d02d72200ef043158a6088d
hahoangviet <hahoangviet@student.utwente.nl>
2020-03-19 12:33:33 +0100

    loadCourse

--- core/src/project12/gameelements/PuttingSimulator.java
+++ core/src/project12/gameelements/PuttingSimulator.java
@@ -222,17 +222,27 @@ public class PuttingSimulator {
      * @param filePath The filepath
      * @return Whether the operation was successfull or not
      */
+//    public boolean loadCourse(String filePath) {
+//        try {
+//            FileInputStream f = new FileInputStream(filePath);
+//            ObjectInputStream inputStream = new ObjectInputStream(f);
+//            course = (PuttingCourse) (inputStream.readObject());
+//            this.ballPosition = course.get_start_position();
+//            inputStream.close();
+//        } catch (IOException | ClassNotFoundException e) {
+//            return false;
+//        }
+//        return true;
+//    }
+    
     public boolean loadCourse(String filePath) {
-        try {
-            FileInputStream f = new FileInputStream(filePath);
-            ObjectInputStream inputStream = new ObjectInputStream(f);
-            course = (PuttingCourse) (inputStream.readObject());
-            this.ballPosition = course.get_start_position();
-            inputStream.close();
-        } catch (IOException | ClassNotFoundException e) {
-            return false;
-        }
-        return true;
+    	Input load = new Input(filePath);
+    	course = load.loadCourse(filePath);
+    	if (course != null) {
+    		this.ballPosition = course.get_start_position();
+    		return true;
+    	}
+    	return false;
     }
 
     public PuttingCourse getCourse() {
