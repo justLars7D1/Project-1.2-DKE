f3a2f85663723b86059ca84229793008932ea581
justLars7D1 <Larsquaedvlieg@outlook.com>
2020-04-28 15:06:04 +0200

    Commented

--- src/main/java/ai/HeuristicBot.java
+++ src/main/java/ai/HeuristicBot.java
@@ -4,6 +4,7 @@ import gameelements.PuttingCourse;
 import gameelements.PuttingSimulator;
 import physicsengine.Vector;
 import physicsengine.Vector3d;
+import physicsengine.engines.EulerSolver;
 import physicsengine.engines.RK4;
 import physicsengine.functions.FunctionParserRPN;
 
@@ -26,40 +27,50 @@ public class HeuristicBot implements PuttingBot {
         // First calculate the direction the shot must have and set it's velocity to the maximum one
         Vector3d shotDirection = flagPosition.minus(ball_position).getNormalized();
 
+        // We create the simulator to take shots with
         PuttingSimulator sim = new PuttingSimulator(course, new RK4(course));
         sim.set_ball_position(ball_position);
         double stepSize = 10e-4;
 
+        // Get the direction of the shot (pointing directly at the flag)
         Vector3d shot = shotDirection.getScaled(maxVelocity);
 
+        // Store the best shot in here
         ShotData bestShot = null;
         double bestTargetDistance = Double.MAX_VALUE;
 
+        // Scaling factor for scaling the shot
         double i = 1;
 
-        double currentDistanceToTarget;
-
+        // Run the loop for 7 seconds
         long startTime = System.nanoTime();
         long timeLimit = 7;
         while ((System.nanoTime() - startTime)/10e8 <= timeLimit) {
 
+            // Simulate the shot
             ShotData resultingPosition = simulateShot(ball_position, sim, shot, stepSize);
 
+            // Calculate the proportion of the start position and the flag to the ball position and the flag
             double ballHoleDistFactor = ballFlagDistance / distanceBetween(ball_position, resultingPosition.getCalculatedPosition());
 
+            // Then we calculate the angle between the start and flag and the ball and flag
             Vector3d startResultVector = resultingPosition.getCalculatedPosition().minus(ball_position);
             Vector3d startFlagVector = flagPosition.minus(ball_position);
             double ballHoleAngle = 180/Math.PI * Math.acos(
                     (startResultVector.dot(startFlagVector)) / (startResultVector.magnitude() * startFlagVector.magnitude())
             );
+
+            // Calculate if the ball is on the left or the right of the flag using the cross product
             Vector3d crossProductStartFlagAndStartSimulated = startFlagVector.cross(resultingPosition.getCalculatedPosition());
 
+            // We scale the shot by the factor we calculated
             shot.scale(Math.pow(ballHoleDistFactor, 1/i));
+
+            // We rotate the shot, compensating for the old "final" position
             shot.rotateYAxis(((crossProductStartFlagAndStartSimulated.get_y() > 0) ? -1 : 1) * ballHoleAngle);
 
-            currentDistanceToTarget = resultingPosition.getDistanceToTarget();
-
-            if (currentDistanceToTarget < bestTargetDistance) {
+            // If it ends up with a better result, we store it
+            if (resultingPosition.getDistanceToTarget() < bestTargetDistance) {
                 bestShot = resultingPosition;
             }
 
@@ -88,7 +99,7 @@ public class HeuristicBot implements PuttingBot {
     }
 
     public static void main(String[] args) {
-        PuttingCourse course = new PuttingCourse(new FunctionParserRPN("-0.01*x + 0.006*x^2 + 0.04 * y"), new Vector3d(0, 10));
+        PuttingCourse course = new PuttingCourse(new FunctionParserRPN("-0.01*x + 0.003*x^2 + 0.04 * y"), new Vector3d(0, 10));
         course.setMaximumVelocity(10);
         PuttingSimulator sim = new PuttingSimulator(course, new RK4(course));
         PuttingBot bot = new HeuristicBot();
