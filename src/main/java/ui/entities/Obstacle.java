57d638966ae4cf34513b7dfcc0e3ff6957993979
justLars7D1 <Larsquaedvlieg@outlook.com>
2020-06-07 14:26:48 +0200

    Example obstacles

---
+++ src/main/java/ui/entities/Obstacle.java
@@ -0,0 +1,5 @@
+package ui.entities;
+
+public interface Obstacle {
+    boolean isHit();
+}
---
+++ src/main/java/ui/entities/obstacles/Wall.java
@@ -0,0 +1,18 @@
+package ui.entities.obstacles;
+
+import org.joml.Vector3f;
+import ui.entities.Entity;
+import ui.entities.Obstacle;
+import ui.models.TexturedModel;
+
+public class Wall extends Entity implements Obstacle {
+
+    public Wall(TexturedModel model, Vector3f position, float scale) {
+        super(model, position, 0, 0, 0, scale);
+    }
+
+    @Override
+    public boolean isHit() {
+        return false;
+    }
+}
