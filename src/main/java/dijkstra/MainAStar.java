bce3905a779a5a51c43c8c20a8edbef31eae1f75
luigiantolini <56222847+luigiantolini@users.noreply.github.com>
2020-06-19 11:13:01 +0200

    AStarMain

---
+++ src/main/java/dijkstra/MainAStar.java
@@ -0,0 +1,30 @@
+package dijkstra;
+
+import gameelements.PuttingCourse;
+import physicsengine.Vector3d;
+import physicsengine.functions.FunctionParserRPN;
+
+import java.util.HashMap;
+
+public class MainAStar {
+/*
+    public static void main(String[] args) {
+        //Graph g0 = ReadFile.read("src/main/java/dijkstra/maze");
+        PuttingCourse course = new PuttingCourse(new FunctionParserRPN("0.0000001*x^2 + 0.0000001*y^2"), new Vector3d(0, 10));
+        Graph g0 = ReadFile.setCoordinates("src/main/java/dijkstra/maze-on-course", course);
+        GraphAL g = ((GraphAL) g0);
+        g.print();
+        System.out.println();
+
+
+    public static void getShortestPathTo(Graph g, int source, int destination) {
+        int[] shortestPathTo = Dijkstra.getShortestPath(g, source, destination);
+        System.out.print(source + " to " + destination + ": ");
+        for (int i = 0; i < shortestPathTo.length; i++) {
+            System.out.print(shortestPathTo[i] + " ");
+        }
+        System.out.println();
+    }*/
+
+}
+
