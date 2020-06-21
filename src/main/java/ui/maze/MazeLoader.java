444542d96918da00e1d50621317639974a8a5351
Adeleimpa <56159423+Adeleimpa@users.noreply.github.com>
2020-06-21 18:17:38 +0200

    Several Mazes

--- src/main/java/ui/maze/MazeLoader.java
+++ src/main/java/ui/maze/MazeLoader.java
@@ -14,6 +14,10 @@ import ui.entities.obstacles.ObstacleFactory;
 import ui.entities.obstacles.Tree;
 import ui.objloader.Vertex;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class MazeLoader {
 
 
@@ -24,9 +28,22 @@ public class MazeLoader {
 
     public static void main(String[] args) {
         PuttingCourse course = new PuttingCourse(new FunctionParserRPN("x + y"), new Vector3d(0, 10));
-        Graph maze = loadMaze("src/main/java/dijkstra/maze-on-course", course);
+
+        List<String> allMazes = new ArrayList<String>();
+        allMazes.add("src/main/java/dijkstra/maze-on-course");
+        allMazes.add("src/main/java/dijkstra/maze-on-course2");
+        allMazes.add("src/main/java/dijkstra/maze-on-course3");
+
+        String chosen = pickOneMaze(allMazes);
+
+        Graph maze = loadMaze(chosen, course);
         ((GraphAL) maze).print();
 
+    }
+
+    public static String pickOneMaze(List<String> allMazes) {
+        Random rand = new Random();
+        return allMazes.get(rand.nextInt(allMazes.size()));
     }
 
 }
