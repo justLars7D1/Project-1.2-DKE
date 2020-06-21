7e4d91c03803cf38828a4bd8abe5f7f7bfe78987
AdaGrund <56160401+AdaGrund@users.noreply.github.com>
2020-06-21 17:11:25 +0200

    maze in UI
    with Adele, Luigi and Stela

--- src/main/java/ui/maze/MazeLoader.java
+++ src/main/java/ui/maze/MazeLoader.java
@@ -7,11 +7,15 @@ import gameelements.PuttingCourse;
 import org.joml.Vector3f;
 import physicsengine.Vector3d;
 import physicsengine.functions.FunctionParserRPN;
+import ui.entities.Obstacle;
 import ui.entities.obstacles.Box;
+import ui.entities.obstacles.ObstacleFactory;
 import ui.entities.obstacles.Tree;
 import ui.objloader.Vertex;
 
 public class MazeLoader {
+
+    private static final ObstacleFactory OBSTACLE_FACTORY =  ObstacleFactory.getFactory() ;
 
     public static Graph loadMaze(String maze_design, PuttingCourse course) {
         Graph maze = ReadFile.setCoordinates(maze_design, course);
@@ -40,17 +44,19 @@ public class MazeLoader {
             Vector3d v = (Vector3d) maze.getValue(i);
             //System.out.println("vertex " + i + " has x=" + v.get_x() + " y=" + v.get_y() + " z=" + v.get_z());
 
-            Vector3f pos1= new Vector3f((float) v.get_x()+2,(float)v.get_y(), (float) v.get_z()+2);
-            Vector3f pos2= new Vector3f((float) v.get_x()-2,(float)v.get_y(), (float) v.get_z()-2);
+           // Vector3f pos1= new Vector3f((float) v.get_x()+2,(float)v.get_y(), (float) v.get_z()+2);
+           // Vector3f pos2= new Vector3f((float) v.get_x()-2,(float)v.get_y(), (float) v.get_z()-2);
 
             for (int neighbour: maze.neighbors(i)){
                 Vector3d n= (Vector3d) maze.getValue(neighbour);
 
+                for (int x= (int) v.get_x(); x<n.get_x(); x++){
+                    Vector3d currentPos= (Vector3d) maze.getValue(x);
+                    Vector3f boxPos = new Vector3f((float) currentPos.get_x() , (float)currentPos.get_y(), (float)currentPos.get_z());
+
+                    Obstacle obstacle = OBSTACLE_FACTORY.createObstacle("box", boxPos, 1);
+                }
             }
-        }
-
-        for(int j=0; j<maze.nodeCount(); j++){
-
         }
 
     }
