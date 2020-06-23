71758eceb101c9b5c6ab8c9bc8832791f2f9d6fe
luigiantolini <56222847+luigiantolini@users.noreply.github.com>
2020-06-23 09:33:53 +0200

    update Astar

--- src/main/java/dijkstra/MainAStar.java
+++ src/main/java/dijkstra/MainAStar.java
@@ -5,9 +5,9 @@ import physicsengine.Vector3d;
 import physicsengine.functions.FunctionParserRPN;
 
 import java.util.HashMap;
+import java.util.List;
 
 public class MainAStar {
-/*
     public static void main(String[] args) {
         //Graph g0 = ReadFile.read("src/main/java/dijkstra/maze");
         PuttingCourse course = new PuttingCourse(new FunctionParserRPN("0.0000001*x^2 + 0.0000001*y^2"), new Vector3d(0, 10));
@@ -15,11 +15,11 @@ public class MainAStar {
         GraphAL g = ((GraphAL) g0);
         g.print();
         System.out.println();
-    
-    //ADDED
-         PuttingCourse course = new PuttingCourse(new FunctionParserRPN("x + y"), new Vector3d(0, 10));
-        Graph g = ReadFile.setCoordinates("src/main/java/dijkstra/maze-on-course", course);
-        ((GraphAL) g).print();
+        for (int i = 0; i < g.nodeCount(); i++) {
+            Vector3d v = (Vector3d) g.getValue(i);
+            System.out.println("vertex " + i + " has x=" + v.get_x() + " y=" + v.get_y() + " z=" + v.get_z());
+        }
+        System.out.println();
         getShortestPathTo(g, 6, 0);
         getShortestPathTo(g, 6, 1);
         getShortestPathTo(g, 6, 3);
@@ -36,17 +36,14 @@ public class MainAStar {
         getShortestPathTo(g, 5, 6);
         getShortestPathTo(g, 5, 7);
         getShortestPathTo(g, 5, 8);
-        
-
+    }
 
     public static void getShortestPathTo(Graph g, int source, int destination) {
-        int[] shortestPathTo = AStar.computePath(g, source, destination); //CHANGED
+        List<Integer> path = AStar.aStar(g,source, destination);
         System.out.print(source + " to " + destination + ": ");
-        for (int i = 0; i < shortestPathTo.length; i++) {
-            System.out.print(shortestPathTo[i] + " ");
+        for (int i = 0; i < path.size(); i++) {
+            System.out.print(path.get(i) + " ");
         }
         System.out.println();
-    }*/
-
+    }
 }
-
