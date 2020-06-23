fbf8455a4ba65b289cee1a59f944c620b17d522a
Adeleimpa <56159423+Adeleimpa@users.noreply.github.com>
2020-06-23 21:48:39 +0200

    position != postition

--- src/main/java/ui/maze/MazeBuilder.java
+++ src/main/java/ui/maze/MazeBuilder.java
@@ -57,21 +57,21 @@ public class MazeBuilder {
         }*/
 
         for (int i = 0; i < maze.nodeCount(); i++) {
-            Vector3d v = (Vector3d) maze.getValue(i);
+            Vector3d v = (Vector3d) maze.getValue(i); // coordinate of vertex
 
             for (int neighbor: maze.neighbors(i)) {
                 Vector3d n = (Vector3d) maze.getValue(neighbor);
-                Vector3d currentNodeToNeighbor = n.minus(v);
-                double length = currentNodeToNeighbor.magnitude();
-                currentNodeToNeighbor.normalize();
+                Vector3d currentNodeToNeighbor = n.minus(v); // subtract the coord of neighbor from the vertex
+                double length = currentNodeToNeighbor.magnitude(); // gets distance between vertex and its neighbor
+                currentNodeToNeighbor.normalize(); // coord becomes 1 1 1
 
                 for (double j = length/2.190922; j < length; j += length/2.190922) {
                     Vector3d firstPoint = currentNodeToNeighbor.getScaled(j);
                     Vector3d rotateLeft = firstPoint.getRotatedYAxis(90).getNormalized().getScaled(2.190922);
-                    Vector3d boxPostitionLeft = v.add(firstPoint).add(rotateLeft);
-                    Vector3d boxPostitionRight = v.add(firstPoint).minus(rotateLeft);
-                    Vector3f uiPositionLeft = new Vector3f((float)boxPostitionLeft.get_x(), (float)boxPostitionLeft.get_y(), (float)boxPostitionLeft.get_z());
-                    Vector3f uiPositionRight = new Vector3f((float)boxPostitionRight.get_x(), (float)boxPostitionRight.get_y(), (float)boxPostitionRight.get_z());
+                    Vector3d boxPositionLeft = v.add(firstPoint).add(rotateLeft);
+                    Vector3d boxPositionRight = v.add(firstPoint).minus(rotateLeft);
+                    Vector3f uiPositionLeft = new Vector3f((float)boxPositionLeft.get_x(), (float)boxPositionLeft.get_y(), (float)boxPositionLeft.get_z());
+                    Vector3f uiPositionRight = new Vector3f((float)boxPositionRight.get_x(), (float)boxPositionRight.get_y(), (float)boxPositionRight.get_z());
                     Obstacle obstacleL = OBSTACLE_FACTORY.createObstacle("box", uiPositionLeft, 1);
                     Obstacle obstacleR = OBSTACLE_FACTORY.createObstacle("box", uiPositionRight, 1);
                     obstacleList.add(obstacleL);
